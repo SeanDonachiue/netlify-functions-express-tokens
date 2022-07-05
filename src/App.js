257a827a538ce7a2d94f02e2769425f6b513f03e
Sean Donachiue <sgdonachiue@gmail.com>
2022-07-05 12:49:05 -0700

    remove react logo and other placeholder junk

--- src/App.js
+++ src/App.js
@@ -4,24 +4,9 @@ import OrderbookDataHook from './OrderbookDataHook.js'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    <OrderbookDataHook/> {/*TODO add inputs to this after datafetch*/}
+      <OrderbookDataHook/> {/*TODO add inputs to this after datafetch*/}
     </div>
   );
 }
 
 export default App;
-
