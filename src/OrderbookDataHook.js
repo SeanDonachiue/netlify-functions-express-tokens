8fa8bc9f090941e146fd09c8e36e1d1e34487a94
Sean Donachiue <sgdonachiue@gmail.com>
2022-07-14 14:31:36 -0700

    url is wrong

--- src/OrderbookDataHook.js
+++ src/OrderbookDataHook.js
@@ -56,7 +56,7 @@ function OrderbookDataHook() {
 				console.log("log it")
 				axios({
 					method: 'get',
-					url: "https://globaltokenbook.netlify.app/.netlify/functions/aggtokenusd",
+					url: "https://master--globaltokenbook.netlify.app/.netlify/functions/aggtokenusd",
 					params: {token : "ethereum"},
 					responseType: 'json',
 					timeout: '10000'
