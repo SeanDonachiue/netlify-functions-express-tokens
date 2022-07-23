29b61f86a4d12bf42b9dfd47c32f7be0385ba17d
Sean Donachiue <sgdonachiue@gmail.com>
2022-07-23 10:18:57 -0700

    strip null values from input data for charts

--- src/OrderbookDataHook.js
+++ src/OrderbookDataHook.js
@@ -68,6 +68,9 @@ function OrderbookDataHook() {
 					let newobdown = [];
 					let newvolume = [];
 					for(let i = 0; i < aggArray.length; i++) {
+						if(aggArray[i].obUp == null || aggArray[i].obDown == null) {
+							continue;
+						}
 						let currDate = new Date(aggArray[i].stamp);
 
 						newtimestamps.push(currDate.getHours() + ":" + currDate.getMinutes() + "-" + currDate.getDate() + "-" + currDate.getMonth() + "-" + currDate.getFullYear())
