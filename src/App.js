740146292c6b14c64974e29b9919ff5c874301a2
Sean Donachiue <sgdonachiue@gmail.com>
2022-08-04 22:03:01 -0700

    inputs work now

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <OrderbookDataHook token="ethereum"/>
+      <OrderbookDataHook token="bitcoin"/>
+      <OrderbookDataHook token="solana"/>
     </div>
   );
 }
